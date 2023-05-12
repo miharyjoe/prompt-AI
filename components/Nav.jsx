1be706ce3fef2b5c334c32edc8a81718f30d0967
miharyjoel <miharyjoel@gmail.com>
2023-05-12 16:38:45 +0300

    feat: nav bar mobile desktop

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,8 @@ const Nav = () => {
 
   const [providers, setProviders] = useState(null);
 
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
   useEffect(() => {
     const setProviders = async () => {
       const response = await getProviders();
@@ -76,8 +78,36 @@ const Nav = () => {
               height={37}
               className="rounded-full"
               alt="profile"
-              onClick={() => {}}
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My profile
+                </Link>{" "}
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
